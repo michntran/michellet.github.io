5f4e81262bc779878d2433d015a12c25597c9194
Michelle Tran <michtran21@gmail.com>
2025-06-25 14:38:48 -0400

    Created pages for portfolio

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+
+    return (
+        <main>
+            <div>
+                <h1>About Michelle!</h1>
+                
+            </div>
+        </main>
+    )
+
+}
---
+++ pages/home.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+
+    return (
+        <main>
+            <div>
+                <h1>Hi, I'm Michelle</h1>
+                <p>I am a University of Virginia graduate with a Bachelor's Degree in
+                Computer Science. I am passionate about  </p>
+            </div>
+        </main>
+    )
+
+}
